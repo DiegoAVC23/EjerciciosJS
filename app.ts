0598a3aea0e7344114b637471fc3168a8c21d57b
DavcDiegoAVC23 <diego_canseco111@outlook.com>
2023-06-07 21:12:41 -0600

    first commit

---
+++ app.ts
@@ -0,0 +1,20 @@
+// const nombre: string = 'Carlos';
+// Declarar sus tipos
+const nombre: string = "John Doe";
+const edad: number = 25;
+console.log(`Hola, ${nombre}. Tienes ${edad} años.`);
+//arreglar el error
+const numero: number = -5;
+
+if (numero > 0) {
+  console.log("El número es positivo");
+} else if (numero === 0) {
+  console.log("El número es cero");
+} else {
+  console.log("El número es negativo");
+}
+const numeros:number[] = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < numeros.length; i++) {
+  console.log(numeros[i]);
+}
